44016cef297c1ebeca9c6b19bfb6881e38bc31d3
Benjamin Arruebarrena <arruebarrena15@gmail.com>
2021-10-03 21:20:53 -0300

    integrando o swagger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const rota = require('./routes/rotas')
 const swaggerUi = require('swagger-ui-express')
-const swaggerJsDoc = require('swagger-js-doc') 
+const swaggerJsDoc = require('swagger-jsdoc') 
 const swaggerOptions = {
     swaggerDefinition: {
         info: {
@@ -18,10 +18,40 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerDocs))
+/**
+ * @swagger
+ * /clientes:
+ *  get:
+ *   description: usado para obter todos os clientes
+ *   responses: 
+ *      '200': 
+ *        description: Com suceso!! 
+ */
 
+app.get('/clientes', (req,res) => {
+    res.send('pagina de clientes')
+})
+
+/**
+ * @swagger
+ * /clientes:
+ *  put:
+ *   description: usado para atualizar os clientes
+ *   responses: 
+ *      '201': 
+ *        description: Com suceso!! 
+ */
+
+app.put('/clientes', (req,res) => {
+    res.status(201).send('consegui atualizar os clientes')
+})
+
+
+app.use('/test', rota)
 app.get('/', (req,res) => {
     res.send('hola mundo')
 
 })
-app.use('/test', rota)
+
 app.listen(3000)
